993d80f8ea5ea456965fa89a63430a86a2141e7d
magdanat <nathanmagdalera@gmail.com>
2019-05-21 03:39:39 -0700

    Add images and SMS functinality

--- app/src/main/java/edu/washington/magdanat/arewethereyet/AlarmReceiver.kt
+++ app/src/main/java/edu/washington/magdanat/arewethereyet/AlarmReceiver.kt
@@ -3,6 +3,7 @@ package edu.washington.magdanat.arewethereyet
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.telephony.SmsManager
 import android.util.Log
 import android.widget.Toast
 
@@ -11,9 +12,13 @@ class AlarmReceiver : BroadcastReceiver() {
 
         val phone = intent?.getStringExtra("phone")
         val message = intent?.getStringExtra("message")
+        val actualPhone = intent?.getStringExtra("phone2")
 
         val toastMessage = "Texting{$phone}: $message"
 
         Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show()
+        val smsManager = SmsManager.getDefault()
+        smsManager.sendTextMessage(actualPhone, null, toastMessage, null, null)
+
     }
 }
--- app/src/main/java/edu/washington/magdanat/arewethereyet/MainActivity.kt
+++ app/src/main/java/edu/washington/magdanat/arewethereyet/MainActivity.kt
@@ -1,14 +1,18 @@
 package edu.washington.magdanat.arewethereyet
 
+import android.Manifest
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +20,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var phone : EditText
     private lateinit var interval : EditText
     private lateinit var button : Button
+    private lateinit var int : Intent
+
+    companion object {
+        const val REQUEST_SMS_SEND_PERMISSION = 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,42 +35,63 @@ class MainActivity : AppCompatActivity() {
         interval = findViewById(R.id.editInterval)
         button = findViewById(R.id.button)
 
-        val alarmSystem = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
         button.setOnClickListener {
             if (button.text == "Start") {
-                if (valid()) {
-                    val phoneNumber = phone.text.toString().replace("\\D+".toRegex(), "")
-                    val area = phoneNumber.substring(0, 3)
-                    val num1 = phoneNumber.substring(3,6)
-                    val num2 = phoneNumber.substring(6)
-                    val reformattedNumber = "($area) $num1-$num2"
-
-                    val intent = Intent(this, AlarmReceiver::class.java).apply {
-                        putExtra("message", message.text.toString())
-                        putExtra("interval", interval.text.toString().toInt())
-                        putExtra("phone", reformattedNumber)
-                    }
-
-                    Log.i("Info", "Alert should start")
-
-                    val newIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-                    val secondInterval = (interval.text.toString().toInt() * 60000).toLong()
-
-                    alarmSystem.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), secondInterval, newIntent)
-
-                    Log.i("Info", "Alert should have been sent")
-
-                    button.text = "Stop"
-                }
+                button.text = "Stop"
+                sendMessages()
             } else {
-                val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT)
-                alarmSystem.cancel(pendingIntent)
                 button.text = "Start"
-                Log.i("Info", "Alert should stop")
+                stopMessages()
             }
         }
+    }
+
+    private fun sendMessages() {
+        val alarmSystem = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            if (valid()) {
+                val phoneNumber = phone.text.toString().replace("\\D+".toRegex(), "")
+                val area = phoneNumber.substring(0, 3)
+                val num1 = phoneNumber.substring(3, 6)
+                val num2 = phoneNumber.substring(6)
+                val reformattedNumber = "($area) $num1-$num2"
+                val reformattedNum2 = "$area$num1$num2"
+                Log.i("Info", reformattedNum2)
+
+                int = Intent(this, AlarmReceiver::class.java).apply {
+                    putExtra("message", message.text.toString())
+                    putExtra("interval", interval.text.toString().toInt())
+                    putExtra("phone", reformattedNumber)
+                    putExtra("phone2", reformattedNum2)
+                }
+
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+                    != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.SEND_SMS),
+                        REQUEST_SMS_SEND_PERMISSION
+                    )
+                } else {
+                    Log.i("Info", "Alert should start")
+                    val newIntent = PendingIntent.getBroadcast(this, 0, int, PendingIntent.FLAG_UPDATE_CURRENT)
+                    val secondInterval = (interval.text.toString().toInt() * 60000).toLong()
+                    alarmSystem.setRepeating(
+                        AlarmManager.RTC_WAKEUP,
+                        System.currentTimeMillis(),
+                        secondInterval,
+                        newIntent
+                    )
+                    Log.i("Info", "Alert should have been sent")
+                }
+        }
+    }
+
+    private fun stopMessages() {
+        val alarmSystem = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val pendingIntent = PendingIntent.getBroadcast(this, 0, int, PendingIntent.FLAG_CANCEL_CURRENT)
+        alarmSystem.cancel(pendingIntent)
+        Log.i("Info", "Alert should stop")
     }
 
     private fun valid(): Boolean {
